334319dcbc8f6c59c9641a5d22e81529781566cd
LagoDeOroIT <lagodeoroit@gmail.com>
2026-01-20 09:15:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -212,7 +212,7 @@ export default function App() {
   </>
 )}
 
-<button
+
                       onClick={() =>
                         openConfirm("Edit this transaction?", () => {
                           setEditingId(t.id);
