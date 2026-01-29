c019225cd07b7426a40ab5ba4950ebdc08da0fb8
LagoDeOroIT <lagodeoroit@gmail.com>
2026-01-29 08:53:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 
 import { createClient } from "@supabase/supabase-js";
 
-export default function App() {
 
 // ================= SUPABASE CONFIG =================
 const supabaseUrl = "https://pmhpydbsysxjikghxjib.supabase.co";
