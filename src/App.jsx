cba30ef76b42f1a9d5ec9449f2ab9ed6ca0c69d1
LagoDeOroIT <lagodeoroit@gmail.com>
2026-02-12 14:29:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-mport React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 // ================= SUPABASE CONFIG =================
