828aa76a5e9f1607398809176170dae45883adc9
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-09 12:22:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import SignUpForm from "./SignUpForm";
-
-// ================= SUPABASE CONFIG =================
-const supabaseUrl = "https://pmhpydbsysxjikghxjib.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtaHB5ZGJzeXN4amlrZ2h4amliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1OTc1NzUsImV4cCI6MjA4NDE3MzU3NX0.AYzyQMAIZKsJFrm4cqv60zmJ76QPk4wKlZdBKkRHsYw";
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { supabase } from "./supabaseClient";
 
 // ================= STYLES =================
 const styles = {
