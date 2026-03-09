ae562ac712ab25d1c2d2809dbe23edf3ba3cb429
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-09 13:52:19 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pmhpydbsysxjikghxjib.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtaHB5ZGJzeXN4amlrZ2h4amliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1OTc1NzUsImV4cCI6MjA4NDE3MzU3NX0.AYzyQMAIZKsJFrm4cqv60zmJ76QPk4wKlZdBKkRHsYw";
+const supabaseUrl = "https://mkfhjklomofrvnnwwknh.supabase.co";
+const supabaseKey = "sb_publishable_h3A6YfbcGgg4A6UeuaDMBA_MUfBbsEU";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
