1d8c71c942b7336cb5a0f2cd638c362657b3d300
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-10 17:48:21 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -264,7 +264,6 @@ export default function App() {
             
             return itemLocation === selected;
           });
-        })
       .map(i => {
    const stock = stockMap[i.id] || 0;
         return {
