ddcf9ea595e9eed257c69237546879a957afa807
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-10 14:31:46 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // ================= SUPABASE CONFIG =================
 const supabaseUrl = "https://mkfhjklomofrvnnwwknh.supabase.co";
-const supabaseKey = "sb_publishable_h3A6YfbcGgg4A6UeuaDMBA_MUfBbsEU";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZmhqa2xvbW9mcnZubnd3a25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTczNzAsImV4cCI6MjA4ODU5MzM3MH0.6Q8p9ms8mnf2daONf7HTP3jGZD_bQuNQrv6cpy0ZUts";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ================= STYLES =================
