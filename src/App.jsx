db8b989c95f804fa53b33981323c23f62bf225e4
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-19 15:36:35 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 import Draggable from "react-draggable";
 // ================= SUPABASE CONFIG =================
-const supabaseUrl = "https://mkfhjklomofrvnnwwknh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZmhqa2xvbW9mcnZubnd3a25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTczNzAsImV4cCI6MjA4ODU5MzM3MH0.6Q8p9ms8mnf2daONf7HTP3jGZD_bQuNQrv6cpy0ZUts";
+const supabaseUrl = "https://ksqdtaeajczxyptazmtr.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzcWR0YWVhamN6eHlwdGF6bXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MDAzODcsImV4cCI6MjA4OTQ3NjM4N30.QNls7uULhk_0-m7ahhvOMAgmCF2ibQ3_TAUt7YD6JuA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 // ================= STYLES =================
 const styles = {
