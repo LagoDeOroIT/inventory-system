df97a6b88c48a0f6289536e251b83e778e014bfa
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-19 14:03:09 +0800

    Delete src/services directory

--- src/services/inventoryService.js
+++
@@ -1,8 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-export const getInventory = async () => {
-  return await supabase
-    .from("inventory")
-    .select("id, name, stock, price")
-    .limit(50);
-};
