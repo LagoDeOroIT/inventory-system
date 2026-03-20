2b999de9ddbc6b21a4d1cb905e6719cdad41bb4a
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-20 09:34:28 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState, useRef, useMemo } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { supabase } from "./services/supabaseClient";
 import Draggable from "react-draggable";
-// ================= SUPABASE CONFIG =================
-const supabaseUrl = "https://ksqdtaeajczxyptazmtr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzcWR0YWVhamN6eHlwdGF6bXRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MDAzODcsImV4cCI6MjA4OTQ3NjM4N30.QNls7uULhk_0-m7ahhvOMAgmCF2ibQ3_TAUt7YD6JuA";
-const supabase = createClient(supabaseUrl, supabaseKey);
+  
 // ================= STYLES =================
 const styles = {
   container:{},
