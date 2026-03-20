a385cd2cf6c8816900175cd26336422f154e3f57
LagoDeOroIT <lagodeoroit@gmail.com>
2026-03-20 15:10:17 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import React, { useEffect, useState, useRef, useMemo } from "react";
 import { createClient } from "@supabase/supabase-js";
 import Draggable from "react-draggable";
